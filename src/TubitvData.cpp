30187cebf59e1749e336b87243c6e0ce47544a5f
Shawn Ray <Asmodasis@gmail.com>
2026-06-25 00:36:50 -0700

    fixed deviceuuid

--- src/TubitvData.cpp
+++ src/TubitvData.cpp
@@ -88,7 +88,7 @@ TubitvData::TubitvData()
 bool TubitvData::Init()
 {
   m_userAgent = kodi::addon::GetSettingString("user_agent", kDefaultUA);
-  m_deviceId  = kodi::addon::GetSettingString("device_id", "");
+  m_deviceId  = kodi::addon::GetSettingString("deviceuuid", "");
 
   if (m_deviceId.empty())
   {
@@ -112,7 +112,7 @@ bool TubitvData::Init()
         break;
     }
     m_deviceId = uuid.str();
-    kodi::addon::SetSettingString("device_id", m_deviceId);
+    kodi::addon::SetSettingString("deviceuuid", m_deviceId);
   }
 
   return true;
