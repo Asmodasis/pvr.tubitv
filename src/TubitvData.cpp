e44f131ac14bac4d6dc584188b008dcd8ee13466
Shawn Ray <Asmodasis@gmail.com>
2026-07-01 23:32:41 -0700

    Update TubitvData.cpp

--- src/TubitvData.cpp
+++ src/TubitvData.cpp
@@ -528,7 +528,7 @@ void TubitvData::ParseProgrammingResponse(const nlohmann::json& j, std::vector<T
     if (ParseRow(jRow, ch))
     {
       
-      ch.m_group.SetGroupName(ch.programs[ch.programs.size()-1].description);
+      ch.m_group.SetGroupName(ch.programs[ch.programs.end()].description);
       
       if (!(std::find(m_genreList.begin(), m_genreList.end(), ch.programs[jRow].description) != m_genreList.end()))
       {
